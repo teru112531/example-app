93a0196b67ba25585ece3f48ded56f471a0f06f4
sail <info@example.com>
2022-09-28 11:09:06 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+//sample
+
+Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class,'show']);
+Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
+
+// tweet
+
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
+	->name('tweet.index');
+Route::middleware('auth')->group(function () { 
+Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
+	->name('tweet.create');
+Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index')->where('tweetId', '[0-9]+');
+Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put')->where('tweetId', '[0-9]+');
+Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
+});
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexCOntroller::class)->name('tweet.index');
+
+require __DIR__.'/auth.php';
